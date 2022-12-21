7a4b825c1467e3184133112475bf13641c19f1a8
nguendh <nguen.dh@phystech.edu>
2022-12-21 05:10:49 +0300

    full api

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,9 @@ from app.forms import LoginForm
 
 from app import api
 from app.api.api_user import GetUser, AddUser, UpdateUser, DeleteUser
+from app.api.api_item import GetItem, AddItem, UpdateItem, DeleteItem
+from app.api.api_order_model import GetOrderModel, AddOrderModel, UpdateOrderModel, DeleteOrderModel
+from app.api.api_items_in_order import GetItemsInOrder, AddItemsInOrder, UpdateItemsInOrder, DeleteItemsInOrder
 
 
 @app.route('/')
@@ -23,10 +26,28 @@ def login():
 
 
 # api route for CRUD user
-api.add_resource(GetUser, '/get')
-api.add_resource(AddUser, '/add')
-api.add_resource(UpdateUser, '/update/<int:id>')
-api.add_resource(DeleteUser, '/delete/<int:id>')
+api.add_resource(GetUser, '/get_user')
+api.add_resource(AddUser, '/add_user')
+api.add_resource(UpdateUser, '/update_user/<int:id>')
+api.add_resource(DeleteUser, '/delete_user/<int:id>')
+
+# api route for CRUD item
+api.add_resource(GetItem, '/get_item')
+api.add_resource(AddItem, '/add_item')
+api.add_resource(UpdateItem, '/update_item/<int:id>')
+api.add_resource(DeleteItem, '/delete_item/<int:id>')
+
+# api route for CRUD items_in_order
+api.add_resource(GetItemsInOrder, '/get_items_in_order')
+api.add_resource(AddItemsInOrder, '/add_items_in_order')
+api.add_resource(UpdateItemsInOrder, '/update_items_in_order/<int:id>')
+api.add_resource(DeleteItemsInOrder, '/delete_items_in_order/<int:id>')
+
+# api route for CRUD order_model
+api.add_resource(GetOrderModel, '/get_order_model')
+api.add_resource(AddOrderModel, '/add_order_model')
+api.add_resource(UpdateOrderModel, '/update_order_model/<int:id>')
+api.add_resource(DeleteOrderModel, '/delete_order_model/<int:id>')
 
 if __name__ == '__main__':
     app.run()
